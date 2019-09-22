17d3c7f6bb3a15eb2ec1c838f9d969200c6d9f36
yansong555 <405620280@qq.com>
2019-09-22 15:50:01 +0800

    first commit

---
+++ init.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Project: guoya-app-test
+# @Author: 小吴老师
+# @Email: wuling@guoyasoft.com
+# @Weichat: 875955899
+# @Create time: 2019/9/22 8:28
+import init_tool
+
+init_tool.init_tools_prj()
